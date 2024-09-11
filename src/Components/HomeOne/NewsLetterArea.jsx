42bb4739bc060f1510a535720bcba42a4cfc25f1
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-09-10 21:04:43 -0400

    css

--- src/Components/HomeOne/NewsLetterArea.jsx
+++ src/Components/HomeOne/NewsLetterArea.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import '../../assets/css/style.css'; // Asegúrate de que la ruta sea correcta
+
 
 function NewsLetterArea() {
   const [isSending, setIsSending] = useState(false);
