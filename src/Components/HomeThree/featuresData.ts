3e9f8a1889fe412776a44ff7c2719cc414c1c73d
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-09-12 16:01:09 -0400

    Update featuresData.ts

--- src/Components/HomeThree/featuresData.ts
+++ src/Components/HomeThree/featuresData.ts
@@ -5,29 +5,49 @@ export interface Feature {
     title: string;
     description: string;
     buttonText: string;
-  }
-  
-  export const features: Feature[] = [
+}
+
+export const features: Feature[] = [
     {
-      id: '1', // Puedes usar ids generados o mantener simples identificadores string
-      icon: '📄',
-      title: 'Online Documentation',
-      description: 'Rapidiously incentivize professional e-tailers and interactive web-readiness',
-      buttonText: 'READ MORE',
+        id: "1", // Puedes usar ids generados o mantener simples identificadores string
+        icon: "📄",
+        title: "Roofing Services",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
     },
     {
-      id: '2',
-      icon: '🎥',
-      title: 'Watch Video Tutorial',
-      description: 'Rapidiously incentivize professional e-tailers and interactive web-readiness',
-      buttonText: 'READ MORE',
+        id: "2",
+        icon: "🎥",
+        title: "Siding Services",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
     },
     {
-      id: '3',
-      icon: '💬',
-      title: 'Dedicated Supports',
-      description: 'Rapidiously incentivize professional e-tailers and interactive web-readiness',
-      buttonText: 'READ MORE',
+        id: "3",
+        icon: "💬",
+        title: "Water Damage",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
     },
-  ];
-  
+    {
+        id: "4",
+        icon: "💬",
+        title: "Wind Damage",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
+    },
+    {
+        id: "5",
+        icon: "💬",
+        title: "Carpentry Services",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
+    },
+    {
+        id: "6",
+        icon: "💬",
+        title: "Painting Services",
+        description: "Rapidiously incentivize professional e-tailers and interactive web-readiness",
+        buttonText: "READ MORE",
+    },
+];
